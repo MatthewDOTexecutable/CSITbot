7142c1337782b130b95ed92f92685a5e193ae1ef
s3667779 <s3667779@student.rmit.edu.au>
2018-08-08 18:05:22 +1000

    Merge branch 'master' of /Users/shadowking98/WebstormProjects/CSITbot with conflicts.

--- Million.js
+++ Million.js
@@ -1,5 +1,16 @@
+const myFunction = (message) => {
+
+    message.delete()
+        .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+        .catch(console.error);
+
+    message.reply('Hey, I\'m a reply!')
+        .then(sent => console.log(`Sent a reply to ${sent.author.username}`))
+        .catch(console.error);
+
+
 async function myFunction () {
 
-}
+};
 
 export default myFunction
