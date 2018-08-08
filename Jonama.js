ea408ba2a24fda81046bae602ac90a8d248b1016
Jonathan <s3719826@student.rmit.edu.au>
2018-08-08 17:35:04 +1000

    Our features

---
+++ Jonama.js
@@ -0,0 +1,16 @@
+bot.on('message', async message => {
+    //ignores message if the sender is a bot
+    if (message.author.bot) return;
+    //checks to see if the first char of the message is the command prefix
+    if (message.content.substring(0, 1) == prefix) {
+        //splits message (after prefix) into two parts, command and args
+        let command = message.content.substring(1).split(' ')[0];
+        let args = message.content.substring(prefix.length + command.length);
+        console.log('command: ' + command + '\nargs: ' + args);
+
+        //example command, reacts with :thinking: if the user types !thonk
+        if (command == 'thonk') {
+            message.react('🤔');
+        }
+    }
+}
