424f10d5f7b6a40f0f9984b3af25706fbde57ce6
Jonathan <s3719826@student.rmit.edu.au>
2018-08-08 17:47:30 +1000

    Our features

--- Jonama.js
+++ Jonama.js
@@ -3,3 +3,14 @@ const myFunction = () => {
 };
 
 export default myFunction
+
+client.on('message', msg => {
+    if (msg.content === '!snek') {
+        message.react('message.guild.emojis.find("snek")');
+    }
+});
+client.on('message', msg => {
+    if (msg.content === '!snek') {
+        message.react('message.guild.emojis.find("snek")');
+    }
+});
